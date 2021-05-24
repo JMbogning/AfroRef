88674700e4a37777d6370660c052e564d3ce99fb
Yamingue kingatal franck <yamking01@gmail.com>
2021-05-24 22:17:04 +0200

    debut map

--- Screens/Map.js
+++ Screens/Map.js
@@ -1,15 +1,54 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-import {  } from 'react-native-elements'
+import { ScrollView, Text, View } from 'react-native'
+import { Header, Image, SearchBar } from 'react-native-elements'
+import color from '../Components/color'
 
- const Mape = (props)=>{
-    return (<View style={{ 
-        flex:1,
-        justifyContent:'center',
-        alignItems: 'center'
-     }}>
-        <Text>mape scren</Text>
-    </View>)
+export default class Mape extends React.Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            search:''
+        }
+    }
+
+    render(){
+        return (<View style={{ 
+            flex:1,
+         }}>
+           <Header
+                    statusBarProps={{ backgroundColor: color.primary }}
+                    barStyle="light-content" // or directly
+                    containerStyle={{
+                        backgroundColor: color.primary,
+                        justifyContent: 'space-around',
+                    }}
+                    centerComponent={{ text: 'Accuille', style: { color: '#fff' } }}
+                    rightComponent={<Image source={require('../assets/logo.png')}
+                        style={{
+                            width: 100,
+                            height: 20,
+                            resizeMode: 'contain',
+                        }}
+                    />}
+                />
+                <SearchBar
+                    lightTheme
+                    containerStyle={{
+                        backgroundColor: color.bacground
+                    }}
+                    inputContainerStyle={{
+                        backgroundColor: '#fff',
+                    }}
+                    placeholder={'Rechercher'}
+                    value={this.state.search}
+                    onChange={(value)=>{this.setState({search:value})}}
+                />
+
+                <ScrollView>
+                    <Image 
+                    source={{ uri: }}
+                    />
+                </ScrollView>
+        </View>)
+    }
 }
-
-export default Mape
